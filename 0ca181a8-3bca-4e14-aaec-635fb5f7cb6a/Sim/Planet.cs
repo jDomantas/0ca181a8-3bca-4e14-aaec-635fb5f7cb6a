22b6f5aae6897cc842bc9494af6c19ae59661f45
RookieMaster <agrookie@gmail.com>
2019-01-26 17:53:23 +0200

    Fix bug with color

--- 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Sim/Planet.cs
+++ 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Sim/Planet.cs
@@ -22,7 +22,7 @@ namespace _0ca181a8_3bca_4e14_aaec_635fb5f7cb6a.Sim
                 Resources.Planets[planetIndex],
                 new Rectangle((int)(Position.X - Radius*1.7), (int)(Position.Y - Radius*1.7),
                 (int)(Radius * 3.4), (int)(Radius * 3.4)),
-                Color.Blue);
+                Color.White);
             
         }
 
