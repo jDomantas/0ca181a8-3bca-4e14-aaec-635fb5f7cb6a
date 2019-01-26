9675d675ddf4f721aad8cd960423d6fa1aa8d4b3
Domantas Jadenkus <djadenkus@gmail.com>
2019-01-26 12:59:01 +0200

    game simulation base

--- 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Resources.cs
+++ 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Resources.cs
@@ -5,6 +5,7 @@ namespace _0ca181a8_3bca_4e14_aaec_635fb5f7cb6a
     static class Resources
     {
         public static Texture2D Pixel;
+        public static Texture2D Circle;
         public static SpriteFont FontArial12;
     }
 }
