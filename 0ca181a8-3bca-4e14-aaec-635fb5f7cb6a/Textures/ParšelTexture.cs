2be06f7dc70252419ee9f4789999a138a37bed28
Paulius <pauliusval99@hotmail.com>
2019-01-26 18:27:25 +0200

    nu tai surenkam

--- 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Textures/ParšelTexture.cs
+++ 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Textures/ParšelTexture.cs
@@ -20,8 +20,7 @@ namespace _0ca181a8_3bca_4e14_aaec_635fb5f7cb6a.Textures
         {
             var angle = angelOffset + Angel;
             var xAxis = Vector.AtAngle(angelOffset);
-            Vector daRealPos = pos + (pos + Offset).Translate(xAxis);
-            
+            Vector daRealPos = pos + Offset.Translate(xAxis);
             sb.Draw(Texture, new Rectangle((int) daRealPos.X, (int) daRealPos.Y, (int) Size.X, (int) Size.Y), null, Color.White, angelOffset + Angel, new Vector2((float) RotationAxis.X, (float) RotationAxis.Y), SpriteEffects.None, 0);
         }
     }
