37bad4e29478e1fa8d601f8f7f267fe8e7c936c6
Domantas Jadenkus <djadenkus@gmail.com>
2019-01-26 10:52:00 +0200

    initial commit

---
+++ 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _0ca181a8_3bca_4e14_aaec_635fb5f7cb6a
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
