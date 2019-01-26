0db6af0712329ff9d5cb7a4e64db5844a772ab8d
RookieMaster <agrookie@gmail.com>
2019-01-26 20:35:38 +0200

    More textures in Resources.cs

--- 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Resources.cs
+++ 0ca181a8-3bca-4e14-aaec-635fb5f7cb6a/Resources.cs
@@ -24,5 +24,18 @@ namespace _0ca181a8_3bca_4e14_aaec_635fb5f7cb6a
         }
         public static Texture2D PlayButton;
         public static Texture2D PlayButtonHover;
+        public static Texture2D LeftButton;
+        public static Texture2D RightButton;
+        public static Texture2D barEmpty;
+        public static Texture2D barFull;
+        public static Texture2D slider;
+        public static Texture2D slider_highlight;
+        public static Texture2D weapons;
+        public static Texture2D ui_base;
+
+
+
+
+
     }
 }
